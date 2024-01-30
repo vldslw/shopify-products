75621ea66e1bd86bce93402629098ecf9698af80
Vladislav Pavlov <vldslw@gmail.com>
2024-01-30 16:52:58 -0300

    set connection to database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const fetchProductsFromShopify = require('./utils/shopify');
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT);
-  fetchProductsFromShopify();
+app.get('/products', (req, res) => {
+  res.json({ message: 'GET products' });
 });
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT);
+      fetchProductsFromShopify();
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
