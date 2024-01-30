08d407d7fcea7a9c89172a1d81d6d0d7643745f9
Vladislav Pavlov <vldslw@gmail.com>
2024-01-30 13:46:33 -0300

    set up prettier, editorconfig

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const express = require('express');
 
 const app = express();
 
 app.get('/api/products', (req, res) => {
-  res.json({message: "GET products"})
+  res.json({ message: 'GET products' });
 });
 
 app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT )
-})
+  console.log('listening on port', process.env.PORT);
+});
