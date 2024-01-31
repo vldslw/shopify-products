bf1362022af77d0af89dbc24266838b24bad0dfe
Vladislav Pavlov <vldslw@gmail.com>
2024-01-31 17:48:47 -0300

    add products fetch on the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState(null);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("http://localhost:4000/api/products");
+      const json = await response.json();
+
+      if (response.ok) {
+        setProducts(json);
+      }
+    };
+    fetchProducts();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="products">
+        {products &&
+          products.map((product) => (
+            <div key={product._id}>
+              <img src={product.src}></img>
+              <div dangerouslySetInnerHTML={{ __html: product.bodyHtml }}></div>
+            </div>
+          ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
