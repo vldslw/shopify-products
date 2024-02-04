aeafd6446f8d4278eb9319c21f3ec2728f6ae136
Vladislav Pavlov <vldslw@gmail.com>
2024-02-04 15:19:10 -0300

    set up cors

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const productRoutes = require('./routes/products');
 const { errorHandler } = require('./middlewares/errorHandler');
+const cors = require('cors');
 
 module.exports = function (database) {
+  const corsOptions = {
+    origin: ['http://localhost:5173'],
+    methods: ['GET'],
+    allowedHeaders: ['Content-Type', 'Origin'],
+  };
+
   const app = express();
 
+  app.use('*', cors(corsOptions));
   app.use(express.json());
 
   // connect to db
